0fd15f58eed87f37aff06aaccb6bf410b0f26c73
AndressaFL <andressadefariaslazoti@gmail.com>
2022-10-04 10:13:32 -0500

    Edit search page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import './Home.css';
 import Background from '../components/Background/Background';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+
 
 function Home() {
     return (
