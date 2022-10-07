e0d28dc789bf8f49822a9936b5097821618eb3d7
AndressaFL <andressadefariaslazoti@gmail.com>
2022-10-07 14:55:47 -0500

    Change bookseat page

--- src/pages/BookingSeat.js
+++ src/pages/BookingSeat.js
@@ -107,8 +107,8 @@ function BookingSeat() {
         </div>
 
         <div className="col-md-9">
-          <div class="h-100 p-5 bg-light border rounded-3">
-            <h1>Information Seat</h1>
+          <div class="h-100 p-4 bg-light border rounded-3 display-7">
+          <h1 class=" lh-1 mb-3"> Seat Information</h1>
             <p>
               Or, keep it light and add a border for some added definition to
               the boundaries of your content. Be sure to look under the hood at
@@ -129,22 +129,21 @@ function BookingSeat() {
           </div>
         </div>
       </div>
-      <div className="container">
-        <div className="row">
-          <div class="col-md-4">
-            <div class="h-100 p-3 bg-light border rounded-3">
-              <h1>Seat Guide</h1>
-              <p>
-                Or, keep it light and add a border for some added definition to
-                the boundaries of your content. Be sure to look under the hood
-                at the source HTML here as we've adjusted the alignment and
-                sizing of both column's content for equal-height.
-              </p>
-            </div>
-          </div>
-        </div>
+      <div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="/images/background.jpeg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/>
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 lh-1 mb-3">Seat Guide</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        
       </div>
     </div>
+  </div>
+    </div>
+
+    
   );
 }
 export default BookingSeat;
