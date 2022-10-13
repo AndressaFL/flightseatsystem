bf9a0279a36cf8dbeaddc7576ddf84caea7ef765
AndressaFL <andressadefariaslazoti@gmail.com>
2022-10-13 11:44:16 -0500

    Connecting to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
+//When you use the MERN stack, you work with React to implement the presentation layer, Express and Node to make up the middle or application layer, and MongoDB to create the database layer.
+//command installs MongoDB database driver that allows your Node.js applications to connect to the database and work with data.
+
+//installs the web framework for Node.js
 const express = require('express');
+const app = express();
+//installs a Node.js package that allows cross origin resource sharing.
+const cors = require("cors");
+//dotenv - installs the module that loads environment variables from a .env file into process.env file. This lets you separate configuration files from the code.
+require("dotenv").config({ path: "./config.env" });
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const app = express();
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+mongoose.connect(process.env.ATLAS_URI);
 
 app.use(bodyParser.json());
 
@@ -29,6 +36,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+
 app.listen(PORT, () => {
+   // perform a database connection when server starts
   console.log(`app running on port ${PORT}`)
 });
