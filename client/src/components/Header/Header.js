0de008f38c981e5a22c77de85f8a9d41ba747586
AndressaFL <andressadefariaslazoti@gmail.com>
2022-10-20 15:02:12 -0500

    Change header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,34 +1,62 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom'
-import { UserContext } from '../../userContext';
-import './Header.css'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../userContext";
+import "./Header.css";
 
 function Header() {
-    const [state, dispatch] = useContext(UserContext);
-    let link = <Link className="me-3 py-2 text-dark text-decoration-none" to="signin">Sign In</Link>;
-    
-    if (state.user) {
-        link = <>
-         <Link className="me-3 py-2 text-dark text-decoration-none" to="chat">Chat</Link>
-        <Link className="me-3 py-2 text-dark text-decoration-none" to="signout">Sign Out</Link>
-       </>;
-    }
+  const [state, dispatch] = useContext(UserContext);
+  let link = (
+    <Link className="me-3 py-2 text-dark text-decoration-none text" to="signin">
+      Sign In
+    </Link>
+  );
 
-    //JSX como o react le e tranforma elementos no DOM
-    return (
-        <header>
-            <div className="d-flex flex-column flex-md-row align-items-center pb-3 border-bottom">
-                <Link to="home" className="d-flex align-items-center text-dark text-decoration-none">
-                    <img width="230" height="30" className="ac-logo-image" alt="Air Canada" src="https://content.achome.digital.aircanada.com/ac/applications/homepage/content/1.0.1009/assets/img/logos/ac/ac_logo.svg" />
-                </Link>
+  if (state.user) {
+    link = (
+      <>
+        <Link
+          className="me-3 py-2 text-dark text-decoration-none"
+          to="searchflight"
+        >
+          Search Flight
+        </Link>
+        <Link className="me-3 py-2 text-dark text-decoration-none" to="chat">
+          Chat
+        </Link>
+        <Link
+          className="me-3 py-2 text-dark text-decoration-none text"
+          to="signout"
+        >
+          Sign Out
+        </Link>
+      </>
+    );
+  }
 
-                <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-                    <Link className="me-3 py-2 text-dark text-decoration-none" to="home">Home</Link>
-                    {link}
-                    
-                </nav>
-            </div>
-        </header>
-    )
+  //JSX como o react le e tranforma elementos no DOM
+  return (
+    <header>
+      <div className="d-flex flex-column flex-md-row align-items-center pb-3 border-bottom">
+        <Link
+          to="home"
+          className="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <img
+            width="230"
+            height="30"
+            className="ac-logo-image"
+            alt="Air Canada"
+            src="https://content.achome.digital.aircanada.com/ac/applications/homepage/content/1.0.1009/assets/img/logos/ac/ac_logo.svg"
+          />
+        </Link>
+        <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+          <Link className="me-3 py-2 text-dark text-decoration-none" to="home">
+            Home
+          </Link>
+          {link}
+        </nav>
+      </div>
+    </header>
+  );
 }
-export default Header
+export default Header;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,24 @@
-import './Home.css'
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <>
-            <div className="row">
-                <div className='col-12 col-md'>
-                    <div className="container home">
-                        <h2 >
-                            It’s more than just a trip.
-                        </h2>
-
-                        <h2 className="information">
-                            Advance Seat Selection<br />
-                            Looking for an aisle or window seat? You can always select your seat free of charge.<br/>
-                            <b>Sign In</b> to find out! <br />
-                        </h2>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="row">
+        <div className="col-12 col-md">
+          <div className="container home">
+            <h2>It’s more than just a trip.</h2>
+            <h2 className="information">
+              Advance Seat Selection
+              <br />
+              Looking for an aisle or window seat? You can always select your
+              seat free of charge.
+              <br />
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Home;
