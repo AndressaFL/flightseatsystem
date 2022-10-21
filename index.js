a6abf3216aa758812b75afa836cd5b1ffbc2543d
AndressaFL <andressadefariaslazoti@gmail.com>
2022-10-20 22:14:39 -0500

    Testing error cors

--- index.js
+++ index.js
@@ -49,4 +49,11 @@ app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 module.exports = app;
