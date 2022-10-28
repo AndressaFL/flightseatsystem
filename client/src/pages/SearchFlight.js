e3e267409a05441eccb491e25d7ac38e09b6dd28
AndressaFL <andressadefariaslazoti@gmail.com>
2022-10-28 12:32:27 -0500

    Change date format

--- client/src/pages/SearchFlight.js
+++ client/src/pages/SearchFlight.js
@@ -3,18 +3,30 @@ import FlightService from "../services/FlightService";
 import "./SearchFlight.css";
 import { useNavigate } from "react-router-dom";
 
+
 function SearchFlight() {
   const [flights, setFlights] = useState([]);
   const navigate = useNavigate();
 
- const handleOnClick = (flightNumber) => {
+  /*new Date().toLocaleString("en-US", {timeZone: "America/New_York"});*/
+
+  const formatDate = (date) => {
+    console.log("got date ", date);
+    const formattedDate = new Date(date).toLocaleString("en-US", {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone});
+    console.log("formatted date ", formattedDate);
+    return formattedDate;
+  };
+
+  const handleOnClick = (flightNumber) => {
     navigate(`/bookseat/${flightNumber}`);
-  }
+  };
 
   useEffect(() => {
     FlightService.find_all()
       .then((response) => {
         setFlights(response.data);
+        
+       
       })
       .catch((e) => {
         console.log("Find flights failed: ", e);
@@ -25,40 +37,48 @@ function SearchFlight() {
       });
   }, []);
 
-  //JSX como o react le e tranforma elementos no DOM
+ 
+
+  /*JSX como o react le e tranforma elementos no DOM*/
   return (
     <>
       <div id="search-form">
         <div id="search">
           <h1>SEARCH YOUR FLIGHT</h1>
         </div>
-        
-          <div className="container">
-            <h1>Flights</h1>
-            <table className="rwd-table center">
-              <tr>
-                <th>Flights</th>
-                <th>From</th>
-                <th>To</th>
-                <th>Departing Time</th>
-                <th>Arriving Time</th>
-                <th></th>
-              </tr>
-              {flights.map((flight) => (
+        <div className="container">
+          <h1>Flights</h1>
+          <table className="rwd-table center">
+            <tr>
+              <th>Flights</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Departing Time</th>
+              <th>Arriving Time</th>
+              <th></th>
+            </tr>
+            {flights.map((flight) => (
                 <tr>
-                  <td key={flight.flightNumber}data-th="Flight Number">{flight.flightNumber}</td>
-                  <td data-th="From">{flight.from}</td>
-                  <td data-th="To">{flight.to}</td>
-                  <td data-th="Departing Time">{flight.departingDate}</td>
-                  <td data-th="Arriving Time">{flight.arrivingDate}</td>
-                  <td data-th="Choose your Seat"><button onClick={() => handleOnClick(flight.flightNumber)} className="btn btn-primary" >
-                Choose your seat
-              </button></td>
-                </tr>
-              ))}
-            </table>
-          </div>
+                <td key={flight.flightNumber} data-th="Flight Number">
+                  {flight.flightNumber}
+                </td>
+                <td data-th="From">{flight.from}</td>
+                <td data-th="To">{flight.to}</td>
+                <td data-th="Departing Time">{formatDate(flight.departingDate)}</td>
+                <td data-th="Arriving Time">{formatDate(flight.arrivingDate)}</td>
+                <td data-th="Choose your Seat">
+                  <button
+                    onClick={() => handleOnClick(flight.flightNumber)}
+                    className="btn btn-primary"
+                  >
+                    Choose your seat
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
+      </div>
     </>
   );
 }
