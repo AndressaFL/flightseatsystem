263ac1a662dd35d7be48b9bbaa67622277da1cbc
AndressaFL <andressadefariaslazoti@gmail.com>
2022-11-29 10:11:12 -0600

    Add service comments

--- client/src/services/FlightService.js
+++ client/src/services/FlightService.js
@@ -1,5 +1,7 @@
 import http from "../backend";
 
+//send a backend request to keep data and use ’em as we need.
+
 class FlightService {
   find(flight_number) {
     return http.get("/flight?flight_id=" + flight_number);
